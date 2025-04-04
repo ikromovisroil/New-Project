a2ab6092eba8dc64339e3e41dff221db3f4e1fc0
Ikromov Isroil <ikromovisroil0309@gmail.com>
2025-04-04 15:30:22 +0500

    12

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
-from django.urls import path,include
-from django.urls import re_path as url
+from django.urls import path, include, re_path
 from django.views.static import serve
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+def redirect_to_admin(request):
+    return redirect('/admin/login/?next=/admin/')
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not settings.DEBUG:
-    urlpatterns += url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    urlpatterns += url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from .models import *
 
-# Create your views here.
+
+def redirect_to_admin(request):
+    return redirect('/admin/login/?next=/admin/')
+
+@login_required
+def index(request):
+    context = {
+        'area': Area.objects.all(),
+        'category': Category.objects.all()
+    }
+    return render(request, 'index.html',context)
